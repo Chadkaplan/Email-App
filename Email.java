1812c887b75a5d048ca5c81e1f3709e3d4c34b72
Chad Kaplan <chadkaplan@knights.ucf.edu>
2020-03-07 00:51:11 -0500

    Pesudocoded most of the project

---
+++ Email.java
@@ -0,0 +1,22 @@
+package emailapp;
+
+public class Email {
+private String firstName;
+private String lastName;
+private String password;
+private String department;
+private int mailboxCapacity;
+private String alternateEmail;
+
+// Constructor for first and last name
+
+// Ask for department
+
+// Generate random password
+
+// Set mailbox capacity
+
+// Set alternate email
+
+// Change password
+}
